68e9f43483fa5a028a2d4b6c7d17db1332d264f5
Akash Shyam <shyamiy@gmail.com>
2021-08-26 13:11:11 +0530

    🚨 fix(lottery): solhint warnings

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,21 +2,20 @@
 pragma solidity ^0.8;
 
 contract Lottery {
-    address public manager;
-    address[] public players;
-    
-    constructor() {
-        manager = msg.sender;
-    }
-    
-    
-    function enter() public payable {
-        require(msg.value > .1 ether);
-        
-        players.push(msg.sender);
-    }
-    
-    function random() private view returns (uint) {
-        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players)));
-    }
+	address public manager;
+	address[] public players;
+
+	constructor() {
+		manager = msg.sender;
+	}
+
+	function enter() public payable {
+		require(msg.value > .1 ether, 'Send at least 0.1 ether.');
+
+		players.push(msg.sender);
+	}
+
+	function random() private view returns (uint256) {
+		return uint256(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players)));
+	}
 }

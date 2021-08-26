61e2ce406a761da979be90985e6820a0a72bc9b7
Akash Shyam <shyamiy@gmail.com>
2021-08-26 20:19:01 +0530

    ✅ test(lottery): pickWinner()

--- tests/Lottery.test.js
+++ tests/Lottery.test.js
@@ -62,4 +62,22 @@ describe('Lottery Contract', () => {
 			assert(true);
 		}
 	});
+
+	it('sends money to winner and resets player array', async () => {
+		await lottery.methods.enter().send({ from: accounts[0], value: web3.utils.toWei('2', 'ether') });
+
+		const initialBalance = await web3.eth.getBalance(accounts[0]);
+
+		await lottery.methods.pickWinner().send({ from: accounts[0] });
+
+		const finalBalance = await web3.eth.getBalance(accounts[0]);
+
+		const difference = finalBalance - initialBalance;
+
+		assert(difference > web3.utils.toWei('1.8', 'ether'));
+
+		const players = await lottery.methods.getPlayers().call({ from: accounts[0] });
+
+		assert.equal(0, players.length);
+	});
 });

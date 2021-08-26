e6dce79fd875b82506070c123d30047603fb9067
Akash Shyam <shyamiy@gmail.com>
2021-08-26 22:44:15 +0530

    🐛 fix: bugs with deploy

--- deploy.js
+++ deploy.js
@@ -3,12 +3,7 @@ const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
 const { PHRASE } = require('./config');
 
-const provider = new HDWalletProvider(
-	PHRASE,
-	// remember to change this to your own phrase!
-	'https://mainnet.infura.io/v3/21e523b9f70746289fb86611a47d199e'
-	// remember to change this to your own endpoint!
-);
+const provider = new HDWalletProvider(PHRASE, 'https://rinkeby.infura.io/v3/21e523b9f70746289fb86611a47d199e');
 const web3 = new Web3(provider);
 
 const deploy = async () => {
@@ -16,11 +11,14 @@ const deploy = async () => {
 
 	console.log('Attempting to deploy from account', accounts[0]);
 
+	console.log(interface);
+
 	const result = await new web3.eth.Contract(JSON.parse(interface))
 		.deploy({ data: bytecode })
 		.send({ gas: '1000000', gasPrice: '5000000000', from: accounts[0] });
 
-	console.log(interface);
+	console.log('RES');
+
 	console.log('Contract deployed to', result.options.address);
 };
 deploy();

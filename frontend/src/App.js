d728548b1df0347b2e2b24cda9331aeea9ca46a1
Akash Shyam <shyamiy@gmail.com>
2021-08-27 12:47:20 +0530

    ✨ feat: form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import web3 from './web3';
+import lottery from './lottery';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [players, setPlayers] = useState([]);
+	const [manager, setManager] = useState(null);
+	const [balance, setBalance] = useState(false);
+	const [amount, setAmount] = useState(0);
+
+	useEffect(() => {
+		(async () => {
+			const managerAddress = await lottery.methods.manager().call();
+			const playersArr = await lottery.methods.getPlayers();
+			const balanceAmt = await web3.eth.getBalance(lottery.options.address);
+
+			setManager(managerAddress);
+			setPlayers(playersArr);
+			setBalance(balanceAmt);
+		})();
+	});
+
+	console.log(manager);
+
+	return (
+		<div>
+			<h2>Lottery</h2>
+			<p>
+				This is hosted by {manager}. There are currently {players.length} players competing for{' '}
+				{web3.utils.fromWei(balance, 'ether')} ether
+			</p>
+
+			<hr />
+			<form>
+				<h4>Want To Try Your Luck?</h4>
+
+				<div>
+					<label htmlFor='ether'>Amount Of Ether</label>
+					<input type='text' id='ether' value={amount} onChange={(e) => setAmount(e.target.value)} />
+				</div>
+			</form>
+		</div>
+	);
 }
 
 export default App;
